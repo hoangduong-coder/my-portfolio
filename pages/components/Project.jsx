b51c2f89fb7814fce982a999426b8defead13359
Hoang Duong <hoangduongphantri@gmail.com>
2022-12-29 00:12:22 +0200

    Develop Work experience page

---
+++ pages/components/Project.jsx
@@ -0,0 +1,18 @@
+const Project = () => {
+  return (
+    <div>
+      <h3>Front-end developer</h3>
+      <p>Internship - Share a Chair, Germany</p>
+      <p>March 2022 - August 2022</p>
+      <ul>
+        <li>TypeScript</li>
+        <li>Next.JS</li>
+        <li>Redux</li>
+        <li>CSS</li>
+        <li>Agile</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Project;
--- pages/dashboard/experience.js
+++ pages/dashboard/experience.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import NavBar from "../components/NavBar";
+import Project from "../components/Project";
 import contentList from "../assets/contents.json"
 import { header } from "../../styles/font";
 
@@ -15,6 +16,7 @@ const Portfolio = () => {
           <h2 style={header.style} className="subHeader">
             {contentList.portfolio.mainHeading}
           </h2>
+          <Project/>
         </div>
       </div>
     </>
